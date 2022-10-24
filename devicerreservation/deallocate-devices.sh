735915ebb326c4452a991c94abc23e4bfee4a1b3
noudysri <53108554+noudysri@users.noreply.github.com>
2022-10-24 13:17:07 +0530

    Update deallocate-devices.sh

--- devicerreservation/deallocate-devices.sh
+++ devicerreservation/deallocate-devices.sh
@@ -1,8 +1,4 @@
 # read the ReservationID-Array : successfulReservedDevices
-
-echo $0
-
-${successfulReservedDevices[@]}=$0
 echo sucessful reserved Devices : ${successfulReservedDevices[@]}
 for key in ${successfulReservedDevices[@]}
 do
